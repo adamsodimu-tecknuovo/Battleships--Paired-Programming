bc3eec3700c717d6483a840ff4df46c1af3f7e50
Natalia Castaneda <nataliacastaneda@Natalias-MacBook-Air.local>
2026-03-16 15:28:53 +0000

    delete data and import data file

--- create_grid_3.py
+++ create_grid_3.py
@@ -1,18 +1,6 @@
 from random import randint
+from data import letters_grid
 
-
-letters_grid = {
-    1 : 'A',
-    2 : 'B',
-    3 : 'C',
-    4 : 'D',
-    5 : 'E',
-    6 : 'F',
-    7 : 'G',
-    8 : 'H',
-    9 : 'I',
-    10 : 'J' 
-    }
 
 num_list = list(range(1,11))
 
