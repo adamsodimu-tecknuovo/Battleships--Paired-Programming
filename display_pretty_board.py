34b7da6633ba5ccda6a7327415b966a17c9ad97b
adamsodimu-tecknuovo <adam.sodimu@tecknuovo.com>
2026-03-19 12:44:00 +0000

    test board

---
+++ display_pretty_board.py
@@ -0,0 +1,22 @@
+from data import letters_grid
+
+
+# This fucntion is aimed to display the user and computer board side by side.
+# The function should print the column letters, A-J and the numbers on the rows, 1-10.
+# The output should be a random column and row. 
+
+test_board = [['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', 'X', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', 'X', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', 'X', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'], ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~']]
+
+def display_pretty_board(board):
+
+    for _ in board:
+        for i in _:
+            print(i, end= " ")
+        print()
+
+display_pretty_board(test_board)
+        
+
+
+
+
